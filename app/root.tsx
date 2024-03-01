21560bf78d6496665eb2ab1c98a0b58e1b745091
stefanovollono <stefano.vollono@bitbull.it>
2024-03-01 11:45:08 +0100

    feat: add storyblok sdk and default boilerplate

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,26 @@ import {
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
 import type { LinksFunction } from "@vercel/remix";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "./components/Feature";
+import Grid from "./components/Grid";
+import Page from "./components/Page";
+import Teaser from "./components/Teaser";
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+storyblokInit({
+  accessToken: process.env.SB_ACCESS_TOKEN,
+  use: [apiPlugin],
+  components,
+  apiOptions: {
+    region: ''
+  }
+});
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,22 @@
-import type { MetaFunction } from "@vercel/remix";
+import Layout from "../components/Layout";
+import type { MetaFunction } from "@remix-run/node";
+import {Link} from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "New Remix" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
+export const loader = async () => {
+  return null;
+};
 
-export default function Index() {
+export default function Page() {
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Layout>
+      index del sito, vai a <Link to="/home">Storyblok</Link>;
+    </Layout>
   );
 }
